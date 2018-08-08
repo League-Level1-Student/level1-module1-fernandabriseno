d270545f3376dbde96f287bfdb51614a78f0a9b6
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-08 16:56:48 -0700

    Duck runner and duck... QUACK!!!

---
+++ src/Duck.java
@@ -0,0 +1,30 @@
+
+
+public class Duck {
+	private static final String String = null;
+	private String favoriteFood;
+	private int numberOfFriends;
+
+	Duck(String favoriteFood, int numberOfFriends) {
+	       this.favoriteFood = favoriteFood;
+	       this.numberOfFriends = numberOfFriends; 
+	       
+	       Dog(String favoriteFood, int numberOfFriends) {
+	           this.favoriteFood = favoriteFood;
+	           this.numberOfFriends = numberOfFriends;
+	    }
+	}
+
+	private void Dog(java.lang.String string2, int numberOfFriends2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void quack() {
+		// TODO Auto-generated method stub
+		
+	}
+}
+    
+
+
