b2473c72ba46a0d9799b3c78e529b82f682e3021
Briseno.Fernanda <Briseno.Fernanda@SEMS-259-20.SEMS259.LAB>
2018-10-10 16:27:05 -0700

    I FINISHED BACKPACK!!!!!!!!!!!!!!!!

---
+++ src/Vault.java
@@ -0,0 +1,11 @@
+
+public class Vault {
+private int secretCode = 952126;  
+public boolean TryCode( int CodeToTry) { 
+if( )CodeToTry == secretCode return true	
+}
+public static void main(String[] args) {
+
+
+}
+}
